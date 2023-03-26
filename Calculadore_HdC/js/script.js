c27b57ff89b9edb16534f7ec4f8ace517607f69f
João Gabriel <joaogabriel9633@gmail.com>
2023-03-26 19:55:10 -0300

    Update README

---
+++ Calculadore_HdC/js/script.js
@@ -0,0 +1,52 @@
+const previousOperationText = document.querySelector('#previous-operations')
+const currentOperationText = document.querySelector('#current-operations')
+const buttons = document.querySelectorAll('#buttons-container button')
+
+class Calculator {
+    constructor(previousOperationText, currentOperationText) {
+        this.previousOperationText = previousOperationText
+        this.currentOperationText = currentOperationText
+        this.currentOperation = ""
+    }
+
+    // add digit to calculator screen
+    addDigit(digit) {
+        // check if current operation already has a dot
+        if(digit === "." && this.currentOperationText.innerText.includes('.')) {
+            return;
+        }
+
+        this.currentOperation = digit
+        this.updateScreen()
+    }
+
+    // Process all calculator operations
+    processOperation(operation) {
+        
+        // Get current and previous value
+        let operationValue
+        let previous = +this.previousOperationText.innerText;
+        let current = +this.currentOperationText.innerText;
+
+    }
+
+    // Change values of the calculator screen
+    updateScreen() {
+        this.currentOperationText.innerText += this.currentOperation;
+    }
+}
+
+const calc = new Calculator(previousOperationText, currentOperationText)
+
+buttons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        const value = e.target.innerText;
+
+        if(+value >= 0 || value === ".") {
+            calc.addDigit(value)
+        } else {
+            calc.processOperation(value)
+        }
+
+    });
+});
