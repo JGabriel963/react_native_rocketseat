f930c8e8b80147f93cbc7cb3bb9f894945b64a6c
João Gabriel <joaogabriel9633@gmail.com>
2023-04-08 23:22:00 -0300

    Project Digital_Clock

---
+++ Digital_Clock/js/script.js
@@ -0,0 +1,31 @@
+let hour = document.getElementById('hour');
+let minute = document.getElementById('minute');
+let second = document.getElementById('seconds');
+console.log({
+    hour,
+    minute,
+    second
+});
+
+let clock = setInterval(
+    function time() {
+
+        let dateNow = new Date();
+        let hr = dateNow.getHours();
+        let min = dateNow.getMinutes();
+        let s = dateNow.getSeconds();
+
+        if (hr < 10) {
+            hr = "0" + hr;
+        }
+        if (s < 10) {
+            s = "0" + s;
+        }
+
+        hour.textContent = hr
+        minute.textContent = min
+        second.textContent = s
+
+    }, 1000
+
+);
