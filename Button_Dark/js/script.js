b87a08a1d0c6cddc6dce2a7e3e3a19e6867ca368
JGabriel963 <joaogabriel9633@gmail.com>
2024-02-06 07:12:47 -0300

    Remove files

--- Button_Dark/js/script.js
+++
@@ -1,7 +0,0 @@
-const body = document.querySelector('body'),
-toggle = document.querySelector('.toggle')
-
-toggle.addEventListener('click', function() {
-    toggle.classList.toggle('active');
-    body.classList.toggle('dark')
-})
--- Random_Background/js/script.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('.button-random').addEventListener('click', function() {
-    document.body.style.backgroundColor = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;
-})
