50d1513d85c16c48ea78f6919ce813284eb4ed41
JGabriel963 <joaogabriel9633@gmail.com>
2024-02-07 21:59:51 -0300

    feat: component is done

--- nlw-expert-notes/src/components/note-card.tsx
+++ nlw-expert-notes/src/components/note-card.tsx
@@ -13,15 +13,13 @@ interface NoteCardProps {
 export function NoteCard({ note }: NoteCardProps) {
   return (
     <Dialog.Root>
-      <Dialog.Trigger asChild>
-        <button className="outline-none rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 relative overflow-hidden hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
-          <span className="text-sm font-medium text-slate-300">
-            {note.date.toISOString()}
-          </span>
-          <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+      <Dialog.Trigger className="outline-none rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 relative overflow-hidden hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+        <span className="text-sm font-medium text-slate-300">
+          {note.date.toISOString()}
+        </span>
+        <p className="text-sm leading-6 text-slate-400">{note.content}</p>
 
-          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-        </button>
+        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50" />
