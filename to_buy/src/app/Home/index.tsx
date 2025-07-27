5e643ecaedee79d834c8ef177a5de306a7aceef5
João Gabriel <joaogabriel9633@gmail.com>
2025-07-27 18:39:14 -0300

    First commit after many

---
+++ to_buy/src/app/Home/index.tsx
@@ -0,0 +1,23 @@
+import { Image, Text, View } from "react-native";
+import { styles } from "./styles";
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+import { Filter } from "@/components/Filter";
+import { FilterStatus } from "@/types/FilterStatus";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={require("@/assets/logo.png")} style={styles.logo} />
+      <View style={styles.form}>
+        <Input placeholder="Digite aqui..." />
+        <Button title="Adicionar" />
+      </View>
+
+      <View style={styles.content}>
+        <Filter status={FilterStatus.DONE} isActive />
+        <Filter status={FilterStatus.PENDING} isActive={false} />
+      </View>
+    </View>
+  );
+}
