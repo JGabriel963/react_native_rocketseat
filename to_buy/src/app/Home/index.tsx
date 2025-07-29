1e718c459b9158644d56f4942fc707fe04a9e599
João Gabriel <joaogabriel9633@gmail.com>
2025-07-29 06:05:38 -0300

    Layout is done

--- to_buy/src/app/Home/index.tsx
+++ to_buy/src/app/Home/index.tsx
@@ -1,9 +1,29 @@
-import { Image, Text, View } from "react-native";
+import { Image, Text, View, TouchableOpacity, FlatList } from "react-native";
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
 import { Filter } from "@/components/Filter";
 import { FilterStatus } from "@/types/FilterStatus";
+import { Item } from "@/components/Item";
+
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
+const ITEMS = [
+  {
+    id: "1",
+    status: FilterStatus.DONE,
+    description: "1 pacote de café",
+  },
+  {
+    id: "2",
+    status: FilterStatus.PENDING,
+    description: "3 pacotes de macarrão",
+  },
+  {
+    id: "3",
+    status: FilterStatus.PENDING,
+    description: "3 cebolas",
+  },
+];
 
 export function Home() {
   return (
@@ -15,8 +35,35 @@ export function Home() {
       </View>
 
       <View style={styles.content}>
-        <Filter status={FilterStatus.DONE} isActive />
-        <Filter status={FilterStatus.PENDING} isActive={false} />
+        <View style={styles.header}>
+          {FILTER_STATUS.map((status) => (
+            <Filter key={status} status={status} isActive />
+          ))}
+          <TouchableOpacity style={styles.clearButton}>
+            <Text style={styles.clearText}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
+
+        <FlatList
+          data={ITEMS}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <Item
+              data={{
+                status: FilterStatus.DONE,
+                description: item.description,
+              }}
+              onRemove={() => {}}
+              onStatus={() => {}}
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          contentContainerStyle={styles.listContent}
+          ListEmptyComponent={() => (
+            <Text style={styles.empty}>Nenhum item aqui.</Text>
+          )}
+        />
       </View>
     </View>
   );
