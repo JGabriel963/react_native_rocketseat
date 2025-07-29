83af5d2814419b3e279689cc8667056c32bedaff
João Gabriel Lenovo <joaogabriel9633@gmail.com>
2025-07-29 10:08:31 -0300

    Fim dos fundamentos

--- to_buy/src/storage/itemsStorage.ts
+++ to_buy/src/storage/itemsStorage.ts
@@ -40,8 +40,43 @@ async function add(newItem: ItemStorage): Promise<ItemStorage[]> {
   return updatedItems;
 }
 
+async function remove(id: string): Promise<void> {
+  const items = await get();
+  const updatedItems = items.filter((item) => item.id !== id);
+  await save(updatedItems);
+}
+
+async function clear(): Promise<void> {
+  try {
+    await AsyncStorage.removeItem(ITEMS_STORAGE_KEY);
+  } catch (error) {
+    throw new Error("ITEMS_CLEAR: " + error);
+  }
+}
+
+async function toggleStatus(id: string): Promise<void> {
+  const items = await get();
+
+  const updatedItems = items.map((item) =>
+    item.id === id
+      ? {
+          ...item,
+          status:
+            item.status === FilterStatus.PENDING
+              ? FilterStatus.DONE
+              : FilterStatus.PENDING,
+        }
+      : item
+  );
+
+  await save(updatedItems);
+}
+
 export const itemsStorage = {
   get,
   getByStatus,
   add,
+  remove,
+  clear,
+  toggleStatus,
 };
